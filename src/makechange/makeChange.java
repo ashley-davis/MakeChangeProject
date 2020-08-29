c5e28ed699f5394de48e5dc41b83c8dbccd76f5c
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-08-29 14:02:32 -0600

    MakeChange

--- src/makechange/makeChange.java
+++ src/makechange/makeChange.java
@@ -3,36 +3,56 @@ package makechange;
 import java.util.Scanner;
 
 public class makeChange {
+	static double paid;
+	static boolean canContinue = true;
+	static Scanner kb = new Scanner(System.in);
+	static double price;
 
 	public static void main(String[] args) {
-		Scanner kb = new Scanner(System.in);
 		System.out.println("Enter price: ");
-		double price = kb.nextDouble();
+		price = kb.nextDouble();
 		System.out.println("Enter amount customer paid: ");
-		double paid = kb.nextInt();
-		double change = price - paid;
-
-		if (change == 0) {
-			System.out.println("This is exact change. ");
-		} else if (paid < price) {
-			System.out.println("This isn't enough money. ");
-		}
-//		while(paid < price) {
-//			System.out.println("Enter an amount greater than " + price);
+		paid = kb.nextInt();
+		double changeOwed = paid - price;
+//		while (canContinue) {
+//			printMenu();
+//			int userInput = kb.nextInt();
+//			if (userInput == 2) {
+//				canContinue = false;
+//			}
 //		}
-//		Start again. Create a method that calls itself or do while loop.
 
-		else {
-			System.out.println("Your change is " + change);
+		if (changeOwed == 0) {
+			exactChange();
+			printMenu();
+
+		}
+		if (price > paid) {
+			notEnough();
 		}
 
-//		calculateChange();
-//Create method to calculate no. of bills/ coins 
-			
-			
-			
+		if (paid > price) {
+			change(changeOwed);
+			printMenu();
+		}
+	}
 
+//		calculateChange() Create method to calculate no. of bills/ coins 
 
-		
-	}}
+	private static void change(double changeOwedParameter) {
+		System.out.println("Your change is " + changeOwedParameter);
+	}
 
+	private static void printMenu() {
+		System.out.println("Press 1 to make change again.\nPress 2 to quit. ");
+
+	}
+
+	private static void exactChange() {
+		System.out.println("This is exact change. ");
+	}
+
+	private static void notEnough() {
+		System.out.println("This isn't enough money. Enter an amount greater than " + paid);
+	}
+}
