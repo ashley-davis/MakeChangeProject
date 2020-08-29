a4e48e42e1eefb4e168db9c7b810e8b9954db900
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-08-29 11:11:41 -0600

    MakeChange

---
+++ src/calcChange/calcChange.java
@@ -0,0 +1,22 @@
+package calcChange;
+
+import java.util.Scanner;
+
+public class calcChange {
+	public static void main(String[] args) {
+		
+	
+	int change=0;
+	int paid = 0;
+	int price=0;
+	change = price-paid;
+	int coins= change<100;
+	
+	if (change<100) {
+		change %.25 =
+	}
+	
+	
+	}
+
+}
--- src/makechange/makeChange.java
+++ src/makechange/makeChange.java
@@ -1,5 +1,38 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class makeChange {
 
-}
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter price: ");
+		double price = kb.nextDouble();
+		System.out.println("Enter amount customer paid: ");
+		double paid = kb.nextInt();
+		double change = price - paid;
+
+		if (change == 0) {
+			System.out.println("This is exact change. ");
+		} else if (paid < price) {
+			System.out.println("This isn't enough money. ");
+		}
+//		while(paid < price) {
+//			System.out.println("Enter an amount greater than " + price);
+//		}
+//		Start again. Create a method that calls itself or do while loop.
+
+		else {
+			System.out.println("Your change is " + change);
+		}
+
+//		calculateChange();
+//Create method to calculate no. of bills/ coins 
+			
+			
+			
+
+
+		
+	}}
+
