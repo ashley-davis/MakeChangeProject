46fafab96b93522d21a4ef9f7e5205846b498488
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-08-30 20:51:42 -0600

    MakeChange

--- src/makechange/makeChange.java
+++ src/makechange/makeChange.java
@@ -1,58 +1,108 @@
 package makechange;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class makeChange {
-	static double paid;
-	static boolean canContinue = true;
-	static Scanner kb = new Scanner(System.in);
-	static double price;
+//Week One Project: A program to receive an amount from the user, calculate change, then show the denominations
+//to be tendered.
+
+public class MakeChange {
+
+	private static DecimalFormat df = new DecimalFormat("0.00");
 
 	public static void main(String[] args) {
-		System.out.println("Enter price: ");
-		price = kb.nextDouble();
-		System.out.println("Enter amount customer paid: ");
-		paid = kb.nextInt();
-		double changeOwed = paid - price;
-//		while (canContinue) {
-//			printMenu();
-//			int userInput = kb.nextInt();
-//			if (userInput == 2) {
-//				canContinue = false;
-//			}
-//		}
-
-		if (changeOwed == 0) {
-			exactChange();
-			printMenu();
-
-		}
+		System.out.println("Welcome to Make Change: Week One Project by Ashley Davis. ");
+		System.out.println("This program calculates correct change at a cash register. ");
+		System.out.println("");
+		System.out.print("Enter price of item: ");
+		Scanner kb = new Scanner(System.in);
+		double price = kb.nextDouble();
+		System.out.println("Enter amount paid by customer: ");
+		double paid = kb.nextDouble();
+		double change = paid - price;
+		//
 		if (price > paid) {
-			notEnough();
+			notEnough(price);
 		}
 
+		if (price == paid) {
+			exactChange();
+		}
 		if (paid > price) {
-			change(changeOwed);
-			printMenu();
+			giveChange(change);
 		}
+//		
+		double remaining = (change * 100);
+
+		int twenties = (int) (remaining / 2000);
+		remaining = (remaining % 2000);
+
+		int tens = (int) (remaining / 1000);
+		remaining = remaining % 1000;
+
+		int fives = (int) (remaining / 500);
+		remaining = remaining % 500;
+//		
+		int dollars = (int) (remaining / 100);
+		remaining = remaining % 100;
+//		
+		int quarters = (int) (remaining / 25);
+		remaining = remaining % 25;
+//		
+		int dimes = (int) (remaining / 10);
+		remaining = remaining % 10;
+//		
+		int nickels = (int) (remaining / 5);
+		remaining = remaining % 5;
+//		
+		double pennies = remaining;
+//		
+
+		if (twenties != 0) {
+			System.out.println(twenties + " twenties ");
+		}
+
+		if (tens != 0) {
+			System.out.println(tens + " tens ");
+		}
+		if (fives != 0) {
+			System.out.println(fives + " fives ");
+		}
+		if (dollars != 0) {
+			System.out.println(dollars + " dollars ");
+
+		}
+		if (quarters != 0) {
+			System.out.println(quarters + " quarters ");
+
+		}
+		if (dimes != 0) {
+			System.out.println(dimes + " dimes ");
+		}
+		if (nickels != 0) {
+			System.out.println(nickels + " nickels ");
+		}
+		if (pennies != 0) {
+			df.setRoundingMode(RoundingMode.UP);
+			System.out.println(df.format(pennies) + " pennies ");
+		}
+
 	}
 
-//		calculateChange() Create method to calculate no. of bills/ coins 
+	private static void giveChange(double change) {
+		System.out.println("Change equals $" + change);
 
-	private static void change(double changeOwedParameter) {
-		System.out.println("Your change is " + changeOwedParameter);
 	}
 
-	private static void printMenu() {
-		System.out.println("Press 1 to make change again.\nPress 2 to quit. ");
-
+	private static void notEnough(double price) {
+		System.out.println("This isn't enough money. Enter an amount greater than " + "$" + price);
 	}
 
 	private static void exactChange() {
 		System.out.println("This is exact change. ");
 	}
 
-	private static void notEnough() {
-		System.out.println("This isn't enough money. Enter an amount greater than " + paid);
-	}
+//		Scanner kb.close;
+
 }
---
+++ src/makechange/makeChange2.java
@@ -0,0 +1,138 @@
+package makechange;
+//Week One Project: A program to receive an amount less than $20 from the user, calculate change, and show the denominations 
+
+//to be tendered.
+
+import java.util.Scanner;
+
+public class makeChangeSkel {
+//	static int paid;
+//	static boolean Loop = true;
+//	static Scanner kb = new Scanner(System.in);
+//	static int price;
+//
+//	public static void main(String[] args) {
+//		System.out.println("Welcome to Make Change by Ashley Davis. ");
+//		System.out.println("Enter purchase price: ");
+//		price = kb.nextInt();
+//		System.out.println("Enter amount paid: ");
+//		paid = kb.nextInt();
+//		double change = paid - price;
+////		while (loop) {
+////			printMenu();
+////			int userInput = kb.nextInt();
+////			if (userInput !=1) {
+////				loop = false;
+////				break;
+////			}
+////		}
+//
+//		if (price == paid) {
+//			exactChange();
+//			printMenu();
+//
+//		}
+//		if (price > paid) {
+//			notEnough();
+//			printMenu();
+//		}
+//
+//		if (price < paid) {
+////			giveChange(change, twenties);
+//			System.out.println("Change is " + change + ".");
+//			int quarters = (int) (change / .25);
+//			System.out.println("That's " + quarters + " quarters. ");
+////			int twenties = (int) (change / 2000);
+////			System.out.println(twenties);
+////			int dimes;
+////			int nickels;
+////			int pennies;
+////			int coins = change;
+////			change = coins;
+////			quarters = coins / 25;
+////			amount = coins % 25;
+////			dimes = coins / 10;
+////			amount = coins % 10;
+////			nickels = coins / 5;
+////			amount = coins % 5;
+////			pennies = coins;
+//
+//			printMenu();
+//		}
+//	}
+//
+//// 	private static void giveChange(double change, int twenties) {
+//// 		System.out.println("Your change is " + change + ".");
+////		System.out.println(twenties);
+////	}
+//
+//	private static void printMenu() {
+//		System.out.println("Press 1 to make change again.\nPress 2 to quit. ");
+//
+//	}
+//
+//	private static void exactChange() {
+//		System.out.println("This is exact change. ");
+//	}
+//
+//	private static void notEnough() {
+//		System.out.println("This isn't enough money. Enter an amount greater than " + paid);
+////	}
+//	public static void main(String[] args) {
+//		double price;
+//		double paid;
+//
+//		System.out.println("price?: ");
+//		Scanner kb = new Scanner(System.in);
+//		price = kb.nextDouble();
+//		System.out.println("paid?: ");
+//		paid = kb.nextDouble();
+//		double change = paid - price;
+//		if (price == paid) {
+//			System.out.println("Exact change.");
+//		}
+//		if (price > paid) {
+//			System.out.println("Not enough money. ");
+//		}
+//		double twenties =10%5;
+////		change=change%20;
+//		if (paid > price) {
+//			System.out.println("Your change is " + change + ". ");
+//			System.out.println("That's " + change);
+//		}
+//	public static void main(String[] args) {
+//		System.out.println("enter number of seconds: ");
+//		Scanner kb = new Scanner(System.in);
+//		int seconds = kb.nextInt();
+////		int seconds = 0;
+//		int minutes = 0;
+//		int hours = 0;
+//		hours = seconds / 3600;
+//		minutes = (seconds / 60) % 60;
+//		seconds = seconds % 60;
+//		System.out.println(hours + " hours " + minutes + " minutes " + seconds + " seconds ");
+//
+////	}
+//	public static void main(String[] args) {
+//		System.out.println("price: ");
+//		System.out.println("$");
+//		Scanner kb=new Scanner(System.in);
+//		double price;
+//		double paid = kb.nextInt();
+//		double change = paid-price;
+//		double tendered=0;
+//		double twenties = 20.00; 
+//		double tens=10.00; 
+//		double fives=5.00;
+//		double ones =1.00;
+//		
+//		if(change >= 20) {
+//			change/20= tendered;
+//		}
+//		
+//		
+//		
+//				
+//	}
+
+}
